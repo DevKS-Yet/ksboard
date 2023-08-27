c107dd48648517b56871f7b3c7ddcdd035145700
DevKS-Yet <ksang0324@gmail.com>
2023-08-27 19:06:40 +0900

    Initial commit

---
+++ src/main/java/com/ksboard/Ksboard1Application.java
@@ -0,0 +1,13 @@
+package com.ksboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ksboard1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ksboard1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksboard/Ksboard1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ksboard1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
